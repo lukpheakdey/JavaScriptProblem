b99ccd3fb9f5ea9b1a449428a835454266bbc970
Luk Pheakdey <luk.pheakdey@gmail.com>
2019-08-30 17:56:18 -0400

    commit triple add

---
+++ 1_triple_add.js
@@ -0,0 +1,9 @@
+
+function tripleAdd(num1){
+    return function(num2){
+        return function(num3){
+            return num1 + num2 + num3;
+        };
+    };
+}
+console.log(tripleAdd(10)(20)(30));
