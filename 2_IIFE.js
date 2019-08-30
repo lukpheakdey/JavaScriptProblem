2ff5eec441fb679e74b4cfb27e5d386c7161f9ac
Luk Pheakdey <luk.pheakdey@gmail.com>
2019-08-30 17:56:38 -0400

    commit IIFE
    what IIFE

---
+++ 2_IIFE.js
@@ -0,0 +1,14 @@
+(function doubleNumber(num) {
+    return num * 2;
+})(10);
+   
+     
+(function() {  
+    function getTotal(a, b) {
+        return a + b;
+    }
+
+    var $ = 'currency';
+
+    if (true) console.log('hello world');
+})()
